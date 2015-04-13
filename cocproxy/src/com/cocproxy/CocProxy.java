c4a514fddf901f804fd803ebcee48fe54ccc12ba
Stefan <Stefan@Stefan-Laptop>
2015-04-14 00:17:17 +0300

    Some changes

--- cocproxy/src/com/cocproxy/CocProxy.java
+++ cocproxy/src/com/cocproxy/CocProxy.java
@@ -1,7 +1,14 @@
 package com.cocproxy;
 
-public class CocProxy {
+import org.apache.log4j.Logger;
 
+public class CocProxy {
+	
+	/** Logger */
+    protected static Logger _log = Logger.getLogger(CocProxy.class);
+    public static final int port = 9339;
+    public static final String originalAddress = "54.226.132.158";
+    
 	public static void main(String[] args) {
 		System.out.println("Starting of the project");
 	}
